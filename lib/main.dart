5a2408fdf3c3a5b0eaf0b67c3e22c3586f2aa42b
Keerthan22-sys <keerthangowdas222@gmail.com>
2022-12-02 02:20:26 +0530

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -15,9 +17,45 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  @override
+  State<DicePage> createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftdice;
+  int rightdice;
+
+  void change() {
+    setState(() {
+      leftdice = Random().nextInt(6) + 1;
+      rightdice = Random().nextInt(6) + 1;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: TextButton(
+              onPressed: () {
+                change();
+              },
+              child: Image.asset('images/dice$leftdice.png'),
+            ),
+          ),
+          Expanded(
+            child: TextButton(
+              onPressed: () {
+                change();
+              },
+              child: Image.asset('images/dice$rightdice.png'),
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
